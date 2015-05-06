c764e5873d2531e449b1bafbf473a924dbe289d8
Kurnevsky Evgeny <kurnevsky@gmail.com>
2015-05-06 23:56:41 +0300

    AtomicOption implemented.

---
+++ src/atomic_option.rs
@@ -0,0 +1,73 @@
+use std::ptr::*;
+use std::sync::atomic::*;
+use std::boxed::*;
+
+#[unsafe_no_drop_flag]
+pub struct AtomicOption<T> {
+    atomic_ptr: AtomicPtr<T>
+}
+
+impl<T> Drop for AtomicOption<T> {
+  fn drop(&mut self) {
+    self.clear(Ordering::Relaxed);
+  }
+}
+
+impl<T> AtomicOption<T> {
+  pub fn new(b: Box<T>) -> AtomicOption<T> {
+    AtomicOption {
+      atomic_ptr: AtomicPtr::new( unsafe { into_raw(b) } )
+    }
+  }
+
+  pub fn empty() -> AtomicOption<T> {
+    AtomicOption {
+      atomic_ptr: AtomicPtr::new(null_mut())
+    }
+  }
+
+  pub fn load<'a>(&'a self, order: Ordering) -> Option<&'a T> {
+    let ptr = self.atomic_ptr.load(order);
+    if !ptr.is_null() {
+      Some(unsafe { &*ptr })
+    } else {
+      None
+    }
+  }
+
+  pub fn store(&self, b: Box<T>, order: Ordering) {
+    let ptr = self.atomic_ptr.swap(unsafe { into_raw(b) }, order);
+    if !ptr.is_null() {
+      let b = unsafe { Box::from_raw(ptr) };
+      drop(b);
+    }
+  }
+
+  pub fn clear(&self, order: Ordering) {
+    let ptr = self.atomic_ptr.swap(null_mut(), order);
+    if !ptr.is_null() {
+      let b = unsafe { Box::from_raw(ptr) };
+      drop(b);
+    }
+  }
+
+  pub fn store_option(&self, b_option: Option<Box<T>>, order: Ordering) {
+    match b_option {
+      Some(b) => self.store(b, order),
+      None => self.clear(order)
+    }
+  }
+
+  pub fn swap(&self, b_option: Option<Box<T>>, order: Ordering) -> Option<Box<T>> {
+    let ptr = match b_option {
+      Some(b) => unsafe { into_raw(b) },
+      None => null_mut()
+    };
+    let old_ptr = self.atomic_ptr.swap(ptr, order);
+    if !old_ptr.is_null() {
+      Some(unsafe { Box::from_raw(old_ptr) })
+    } else {
+      None
+    }
+  }
+}
--- src/uct.rs
+++ src/uct.rs
@@ -1,11 +1,37 @@
+use std::iter::*;
 use std::sync::atomic::*;
+use atomic_option::*;
 use types::*;
+use player::*;
 
 struct UctNode {
   wins: AtomicUsize,
   draws: AtomicUsize,
   visits: AtomicUsize,
   pos: Pos,
-  child: AtomicPtr<UctNode>,
+  child: AtomicOption<UctNode>,
   sibling: Option<Box<UctNode>>
+}
+
+pub struct UctRoot {
+  node: Option<Box<UctNode>>,
+  moves: Vec<Pos>,
+  moves_field: Vec<bool>,
+  player: Player,
+  points_seq: Vec<Pos>
+}
+
+impl UctRoot {
+  pub fn new(length: Pos) -> UctRoot {
+    UctRoot {
+      node: None,
+      moves: Vec::new(),
+      moves_field: repeat(false).take(length).collect(),
+      player: Player::Red,
+      points_seq: Vec::new()
+    }
+  }
+
+  //pub fn best_move
+  //pub fn estimates
 }
