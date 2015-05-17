d3142b93d83e990fe5d4a0fa35dfe02111d09a46
Kurnevsky Evgeny <kurnevsky@gmail.com>
2015-05-17 16:32:33 +0300

    UCB implemented.

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,14 @@
 use types::*;
 
-pub static UCT_RADIUS: CoordSum = 8;
+pub enum UcbType {
+  Ucb1,
+  Ucb1Tuned
+}
+
+pub static UCT_RADIUS: CoordSum = 3;
+
+pub static UCB_TYPE: UcbType = UcbType::Ucb1Tuned;
+
+pub static UCT_DRAW_WEIGHT: f32 = 0.4;
+
+pub static UCTK: f32 = 1.0;
