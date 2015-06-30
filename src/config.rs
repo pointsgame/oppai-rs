f73ec1c7794dcb5e1b1877477e8fe20f2738a07b
Kurnevsky Evgeny <kurnevsky@gmail.com>
2015-06-30 21:23:03 +0300

    Static komi enabled by default.

--- src/config.rs
+++ src/config.rs
@@ -39,7 +39,7 @@ static UCT_DEPTH: Depth = 8;
 
 static mut THREADS_COUNT: usize = 4;
 
-static UCT_KOMI_TYPE: UctKomiType = UctKomiType::None;
+static UCT_KOMI_TYPE: UctKomiType = UctKomiType::Static;
 
 static UCT_RED: f32 = 0.45;
 
