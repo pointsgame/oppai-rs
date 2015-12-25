e5c8dd6a6d2e461d16500c0b8697245b7ed10136
Kurnevsky Evgeny <kurnevsky@gmail.com>
2015-12-25 15:12:14 +0300

    Patterns reformatting.

--- src/dfa.rs
+++ src/dfa.rs
@@ -4,201 +4,201 @@ use player::Player;
 use cell::Cell;
 
 pub struct DfaState {
-    empty: i32,
-    red: i32,
-    black: i32,
-    bad: i32,
-    pattern: i32
+  empty: i32,
+  red: i32,
+  black: i32,
+  bad: i32,
+  pattern: i32
 }
 
 impl DfaState {
-    pub fn new(empty: i32, red: i32, black: i32, bad: i32, pattern: i32) -> DfaState {
-        DfaState {
-            empty: empty,
-            red: red,
-            black: black,
-            bad: bad,
-            pattern: pattern
-        }
+  pub fn new(empty: i32, red: i32, black: i32, bad: i32, pattern: i32) -> DfaState {
+    DfaState {
+      empty: empty,
+      red: red,
+      black: black,
+      bad: bad,
+      pattern: pattern
     }
+  }
 }
 
 pub struct Dfa {
-    states: Vec<DfaState>
+  states: Vec<DfaState>
 }
 
 impl Dfa {
-    pub fn new(states: Vec<DfaState>) -> Dfa {
-        Dfa {
-            states: states
-        }
+  pub fn new(states: Vec<DfaState>) -> Dfa {
+    Dfa {
+      states: states
     }
+  }
 
-    pub fn product(&self, other: &Dfa) -> Dfa {
-        let other_len = other.states.len();
-        let other_len_i32 = other_len as i32;
-        let mut new_states = Vec::with_capacity(self.states.len() * other_len);
-        for self_state in &self.states {
-            let base_empty = (self_state.empty + 1) * other_len_i32;
-            let base_red = (self_state.red + 1) * other_len_i32;
-            let base_black = (self_state.black + 1) * other_len_i32;
-            let base_bad = (self_state.bad + 1) * other_len_i32;
-            for other_state in &other.states {
-                let new_state = DfaState {
-                    empty: base_empty + other_state.empty,
-                    red: base_red + other_state.red,
-                    black: base_black + other_state.black,
-                    bad: base_bad + other_state.bad,
-                    pattern: if self_state.pattern != -1 { self_state.pattern } else { other_state.pattern }
-                };
-                new_states.push(new_state);
-            }
-        }
-        Dfa {
-            states: new_states
-        }
+  pub fn product(&self, other: &Dfa) -> Dfa {
+    let other_len = other.states.len();
+    let other_len_i32 = other_len as i32;
+    let mut new_states = Vec::with_capacity(self.states.len() * other_len);
+    for self_state in &self.states {
+      let base_empty = (self_state.empty + 1) * other_len_i32;
+      let base_red = (self_state.red + 1) * other_len_i32;
+      let base_black = (self_state.black + 1) * other_len_i32;
+      let base_bad = (self_state.bad + 1) * other_len_i32;
+      for other_state in &other.states {
+        let new_state = DfaState {
+          empty: base_empty + other_state.empty,
+          red: base_red + other_state.red,
+          black: base_black + other_state.black,
+          bad: base_bad + other_state.bad,
+          pattern: if self_state.pattern != -1 { self_state.pattern } else { other_state.pattern }
+        };
+        new_states.push(new_state);
+      }
     }
+    Dfa {
+      states: new_states
+    }
+  }
 
-    pub fn run(&self, iter: &mut Iterator<Item = Cell>) -> Option<u32> {
-        let mut state_idx = 0i32;
-        loop {
-            let state = &self.states[state_idx as usize];
-            if state.pattern != -1 {
-                return Some(state.pattern as u32);
-            }
-            if let Some(cell) = iter.next() {
-                if cell.is_bad() {
-                    state_idx = state.bad;
-                } else if let Some(player) = cell.get_owner() {
-                    match player {
-                        Player::Red => state_idx = state.red,
-                        Player::Black => state_idx = state.black
-                    }
-                } else {
-                    state_idx = state.empty;
-                }
-            } else {
-                return None;
-            }
-            if state_idx == -1 {
-                return None;
-            }
+  pub fn run(&self, iter: &mut Iterator<Item = Cell>) -> Option<u32> {
+    let mut state_idx = 0i32;
+    loop {
+      let state = &self.states[state_idx as usize];
+      if state.pattern != -1 {
+        return Some(state.pattern as u32);
+      }
+      if let Some(cell) = iter.next() {
+        if cell.is_bad() {
+          state_idx = state.bad;
+        } else if let Some(player) = cell.get_owner() {
+          match player {
+            Player::Red => state_idx = state.red,
+            Player::Black => state_idx = state.black
+          }
+        } else {
+          state_idx = state.empty;
         }
+      } else {
+        return None;
+      }
+      if state_idx == -1 {
+        return None;
+      }
     }
+  }
 
-    fn delete_states(&mut self, states_for_delete: Vec<u32>) {
-        let mut shifts = Vec::with_capacity(self.states.len());
-        for sd in &states_for_delete {
-            let shift = shifts.last().map_or(0, |&d| d) + sd;
-            shifts.push(shift);
-        }
-        let deletions = shifts.last().map_or(0, |&d| d);
-        if deletions == 0 {
-            return;
-        }
-        let mut new_states = Vec::with_capacity(self.states.len() - deletions as usize);
-        for state in self.states.iter().zip(states_for_delete.into_iter()).filter_map(|(state, sd)| if sd == 0 { Some(state) } else { None }) {
-            let new_state = DfaState {
-                empty: if state.empty == -1 { -1 } else { state.empty - shifts[state.empty as usize] as i32 },
-                red: if state.red == -1 { -1 } else { state.red - shifts[state.red as usize] as i32 },
-                black: if state.black == -1 { -1 } else { state.black - shifts[state.black as usize] as i32 },
-                bad: if state.bad == -1 { -1 } else { state.bad - shifts[state.bad as usize] as i32 },
-                pattern: state.pattern
-            };
-            new_states.push(new_state);
-        }
-        self.states = new_states;
+  fn delete_states(&mut self, states_for_delete: Vec<u32>) {
+    let mut shifts = Vec::with_capacity(self.states.len());
+    for sd in &states_for_delete {
+      let shift = shifts.last().map_or(0, |&d| d) + sd;
+      shifts.push(shift);
     }
+    let deletions = shifts.last().map_or(0, |&d| d);
+    if deletions == 0 {
+      return;
+    }
+    let mut new_states = Vec::with_capacity(self.states.len() - deletions as usize);
+    for state in self.states.iter().zip(states_for_delete.into_iter()).filter_map(|(state, sd)| if sd == 0 { Some(state) } else { None }) {
+      let new_state = DfaState {
+        empty: if state.empty == -1 { -1 } else { state.empty - shifts[state.empty as usize] as i32 },
+        red: if state.red == -1 { -1 } else { state.red - shifts[state.red as usize] as i32 },
+        black: if state.black == -1 { -1 } else { state.black - shifts[state.black as usize] as i32 },
+        bad: if state.bad == -1 { -1 } else { state.bad - shifts[state.bad as usize] as i32 },
+        pattern: state.pattern
+      };
+      new_states.push(new_state);
+    }
+    self.states = new_states;
+  }
 
-    pub fn delete_non_reachable(&mut self) {
-        let mut non_reachable = iter::repeat(1).take(self.states.len()).collect::<Vec<u32>>();
-        let mut q = VecDeque::with_capacity(self.states.len());
-        q.push_back(1);
-        while let Some(idx) = q.pop_front() {
-            let state = &self.states[idx as usize];
-            if state.empty != -1 && non_reachable[state.empty as usize] == 1 {
-                non_reachable[state.empty as usize] = 0;
-                q.push_back(state.empty);
-            }
-            if state.red != -1 && non_reachable[state.red as usize] == 1 {
-                non_reachable[state.red as usize] = 0;
-                q.push_back(state.red);
-            }
-            if state.black != -1 && non_reachable[state.black as usize] == 1 {
-                non_reachable[state.black as usize] = 0;
-                q.push_back(state.black);
-            }
-            if state.bad != -1 && non_reachable[state.bad as usize] == 1 {
-                non_reachable[state.bad as usize] = 0;
-                q.push_back(state.bad);
-            }
-        }
-        self.delete_states(non_reachable);
+  pub fn delete_non_reachable(&mut self) {
+    let mut non_reachable = iter::repeat(1).take(self.states.len()).collect::<Vec<u32>>();
+    let mut q = VecDeque::with_capacity(self.states.len());
+    q.push_back(1);
+    while let Some(idx) = q.pop_front() {
+      let state = &self.states[idx as usize];
+      if state.empty != -1 && non_reachable[state.empty as usize] == 1 {
+        non_reachable[state.empty as usize] = 0;
+        q.push_back(state.empty);
+      }
+      if state.red != -1 && non_reachable[state.red as usize] == 1 {
+        non_reachable[state.red as usize] = 0;
+        q.push_back(state.red);
+      }
+      if state.black != -1 && non_reachable[state.black as usize] == 1 {
+        non_reachable[state.black as usize] = 0;
+        q.push_back(state.black);
+      }
+      if state.bad != -1 && non_reachable[state.bad as usize] == 1 {
+        non_reachable[state.bad as usize] = 0;
+        q.push_back(state.bad);
+      }
     }
+    self.delete_states(non_reachable);
+  }
 
-    fn pyramid_idx_base(i: usize) -> usize {
-        i * (i - 1) / 2
-    }
+  fn pyramid_idx_base(i: usize) -> usize {
+    i * (i - 1) / 2
+  }
 
-    fn pyramid_idx(i: usize, j: usize) -> usize {
-        Dfa::pyramid_idx_base(i) + j
-    }
+  fn pyramid_idx(i: usize, j: usize) -> usize {
+    Dfa::pyramid_idx_base(i) + j
+  }
 
-    pub fn minimize(&mut self) {
-        let len = self.states.len();
-        let mut not_equal = iter::repeat(0).take(len * (len - 1) / 2 + len - 1).collect::<Vec<u32>>();
-        for (i, pattern_i) in self.states.iter().enumerate().skip(1) {
-            let base = Dfa::pyramid_idx_base(i);
-            for (j, pattern_j) in self.states[.. i - 1].iter().enumerate() {
-                if pattern_i.pattern != pattern_j.pattern {
-                    not_equal[base + j] = 1;
-                }
-            }
+  pub fn minimize(&mut self) {
+    let len = self.states.len();
+    let mut not_equal = iter::repeat(0).take(len * (len - 1) / 2 + len - 1).collect::<Vec<u32>>();
+    for (i, pattern_i) in self.states.iter().enumerate().skip(1) {
+      let base = Dfa::pyramid_idx_base(i);
+      for (j, pattern_j) in self.states[.. i - 1].iter().enumerate() {
+        if pattern_i.pattern != pattern_j.pattern {
+          not_equal[base + j] = 1;
         }
-        'outer: loop {
-            for (i, pattern_i) in self.states.iter().enumerate().skip(1) {
-                let base = Dfa::pyramid_idx_base(i);
-                for (j, pattern_j) in self.states[.. i - 1].iter().enumerate() {
-                    let idx = base + j;
-                    if not_equal[idx] == 0 {
-                        if pattern_i.empty != pattern_j.empty && (pattern_i.empty == -1 || pattern_j.empty == -1 || not_equal[Dfa::pyramid_idx(pattern_i.empty as usize, pattern_j.empty as usize)] == 1) ||
-                            pattern_i.red != pattern_j.red && (pattern_i.red == -1 || pattern_j.red == -1 || not_equal[Dfa::pyramid_idx(pattern_i.red as usize, pattern_j.red as usize)] == 1) ||
-                            pattern_i.black != pattern_j.black && (pattern_i.black == -1 || pattern_j.black == -1 || not_equal[Dfa::pyramid_idx(pattern_i.black as usize, pattern_j.black as usize)] == 1) ||
-                            pattern_i.bad != pattern_j.bad && (pattern_i.bad == -1 || pattern_j.bad == -1 || not_equal[Dfa::pyramid_idx(pattern_i.bad as usize, pattern_j.black as usize)] == 1) {
-                            not_equal[idx] = 1;
-                            continue 'outer;
-                        }
-                    }
-                }
-            }
-            break;
-        }
-        let mut deleted = iter::repeat(0).take(self.states.len()).collect::<Vec<u32>>();
-        for i in 1 .. self.states.len() {
-            let base = Dfa::pyramid_idx_base(i);
-            let i_i32 = i as i32;
-            for j in 0 .. i - 1 {
-                let idx = base + j;
-                if not_equal[idx] == 0 && deleted[j] == 0 {
-                    let j_i32 = j as i32;
-                    for state in &mut self.states {
-                        if state.empty == i_i32 {
-                            state.empty = j_i32;
-                        }
-                        if state.red == i_i32 {
-                            state.red = j_i32;
-                        }
-                        if state.black == i_i32 {
-                            state.black = j_i32;
-                        }
-                        if state.bad == i_i32 {
-                            state.bad = j_i32;
-                        }
-                    }
-                    deleted[i] = 1;
-                }
-            }
-        }
-        self.delete_states(deleted);
+      }
     }
+    'outer: loop {
+      for (i, pattern_i) in self.states.iter().enumerate().skip(1) {
+        let base = Dfa::pyramid_idx_base(i);
+        for (j, pattern_j) in self.states[.. i - 1].iter().enumerate() {
+          let idx = base + j;
+          if not_equal[idx] == 0 {
+            if pattern_i.empty != pattern_j.empty && (pattern_i.empty == -1 || pattern_j.empty == -1 || not_equal[Dfa::pyramid_idx(pattern_i.empty as usize, pattern_j.empty as usize)] == 1) ||
+               pattern_i.red != pattern_j.red && (pattern_i.red == -1 || pattern_j.red == -1 || not_equal[Dfa::pyramid_idx(pattern_i.red as usize, pattern_j.red as usize)] == 1) ||
+               pattern_i.black != pattern_j.black && (pattern_i.black == -1 || pattern_j.black == -1 || not_equal[Dfa::pyramid_idx(pattern_i.black as usize, pattern_j.black as usize)] == 1) ||
+               pattern_i.bad != pattern_j.bad && (pattern_i.bad == -1 || pattern_j.bad == -1 || not_equal[Dfa::pyramid_idx(pattern_i.bad as usize, pattern_j.black as usize)] == 1) {
+              not_equal[idx] = 1;
+              continue 'outer;
+            }
+          }
+        }
+      }
+      break;
+    }
+    let mut deleted = iter::repeat(0).take(self.states.len()).collect::<Vec<u32>>();
+    for i in 1 .. self.states.len() {
+      let base = Dfa::pyramid_idx_base(i);
+      let i_i32 = i as i32;
+      for j in 0 .. i - 1 {
+        let idx = base + j;
+        if not_equal[idx] == 0 && deleted[j] == 0 {
+          let j_i32 = j as i32;
+          for state in &mut self.states {
+            if state.empty == i_i32 {
+              state.empty = j_i32;
+            }
+            if state.red == i_i32 {
+              state.red = j_i32;
+            }
+            if state.black == i_i32 {
+              state.black = j_i32;
+            }
+            if state.bad == i_i32 {
+              state.bad = j_i32;
+            }
+          }
+          deleted[i] = 1;
+        }
+      }
+    }
+    self.delete_states(deleted);
+  }
 }
--- src/patterns.rs
+++ src/patterns.rs
@@ -8,120 +8,119 @@ use dfa::{Dfa, DfaState};
 use field::Field;
 
 struct Move {
-    x: u32,
-    y: u32,
-    p: f64 // probability
+  x: u32,
+  y: u32,
+  p: f64 // probability
 }
 
 struct Pattern {
-    p: f64, // priority (probability = p / sum(p))
-    moves: Vec<Move>
+  p: f64, // priority (probability = p / sum(p))
+  moves: Vec<Move>
 }
 
 struct Patterns {
-    dfa: Dfa,
-    patterns: Vec<Pattern>
+  dfa: Dfa,
+  patterns: Vec<Pattern>
 }
 
 impl Patterns {
-    fn read_header<T: BufRead>(input: &mut T, s: &mut String) -> (u32, u32, u32, f64) {
-        s.clear();
-        input.read_line(s).ok();
-        let mut split = s.split(' ').fuse();
-        let width = u32::from_str(split.next().expect("Invalid pattern format: expected width.")).expect("Invalid pattern format: width must be u32."); //TODO: validate all this.
-        let height = u32::from_str(split.next().expect("Invalid pattern format: expected height.")).expect("Invalid pattern format: height must be u32.");
-        let moves_count = u32::from_str(split.next().expect("Invalid pattern format: expected moves count.")).expect("Invalid pattern format: moves count must be u32.");
-        let priority = f64::from_str(split.next().expect("Invalid pattern format: expected priority.")).expect("Invalid pattern format: priority must be f64.");
-        (width, height, moves_count, priority)
-    }
+  fn read_header<T: BufRead>(input: &mut T, s: &mut String) -> (u32, u32, u32, f64) {
+    s.clear();
+    input.read_line(s).ok();
+    let mut split = s.split(' ').fuse();
+    let width = u32::from_str(split.next().expect("Invalid pattern format: expected width.")).expect("Invalid pattern format: width must be u32."); //TODO: validate all this.
+    let height = u32::from_str(split.next().expect("Invalid pattern format: expected height.")).expect("Invalid pattern format: height must be u32.");
+    let moves_count = u32::from_str(split.next().expect("Invalid pattern format: expected moves count.")).expect("Invalid pattern format: moves count must be u32.");
+    let priority = f64::from_str(split.next().expect("Invalid pattern format: expected priority.")).expect("Invalid pattern format: priority must be f64.");
+    (width, height, moves_count, priority)
+  }
 
-    fn read_pattern<T: BufRead>(input: &mut T, s: &mut String, width: u32, height: u32) {
-        s.clear();
-        for y in 0 .. height {
-            input.read_line(s).ok(); //TODO: check sizes.
+  fn read_pattern<T: BufRead>(input: &mut T, s: &mut String, width: u32, height: u32) {
+    s.clear();
+    for y in 0 .. height {
+      input.read_line(s).ok(); //TODO: check sizes.
+    }
+  }
+
+  fn read_moves<T: BufRead>(input: &mut T, s: &mut String, moves_count: u32) -> Vec<Move> {
+    unimplemented!()
+  }
+
+  fn covering_spiral_length(side_of_square: u32) -> u32 {
+    let x = side_of_square / 2;
+    (8 * x - 13) * x + 6
+  }
+
+  fn build_dfa(width: u32, height: u32, pattern: u32, s: &str) -> Dfa {
+    let center_x = width / 2;
+    let center_y = height / 2;
+    let spiral_length = Patterns::covering_spiral_length(cmp::max(width, height)) as usize;
+    let mut states = Vec::with_capacity(spiral_length + 1);
+    let mut i = 0;
+    for (shift_x, shift_y) in Spiral::new().into_iter().take(spiral_length) {
+      i += 1;
+      let x = center_x as i32 + shift_x;
+      let y = center_y as i32 + shift_y;
+      let state = if x >= 0 && x < width as i32 && y >= 0 && y < height as i32 {
+        let pos = y as u32 * width + x as u32;
+        match s.char_at(pos as usize) {
+          '.' => DfaState::new(i, -1, -1, -1, -1),
+          '?' => DfaState::new(i, i, i, i, -1),
+          'R' => DfaState::new(-1, i, -1, -1, -1),
+          'B' => DfaState::new(-1, -1, i, -1, -1),
+          'r' => DfaState::new(i, i, -1, -1, -1),
+          'b' => DfaState::new(i, -1, i, -1, -1),
+          '*' => DfaState::new(-1, -1, -1, i, -1),
+          c   => panic!("Invalid character in pattern: {}", c)
         }
+      } else {
+        DfaState::new(-1, -1, -1, -1, -1)
+      };
+      states.push(state);
     }
+    states.push(DfaState::new(-1, -1, -1, -1, pattern as i32));
+    Dfa::new(states)
+  }
 
-    fn read_moves<T: BufRead>(input: &mut T, s: &mut String, moves_count: u32) -> Vec<Move> {
-        unimplemented!()
+  pub fn load(file: File) -> Patterns {
+    let archive = Archive::new(file);
+    let mut s = String::new();
+    let mut patterns = Vec::new();
+    let mut iter = archive.files().expect("Reading of tar archive is failed.").into_iter().map(|file| file.expect("Reading of file in tar archive is failed."));
+    let mut dfa = {
+      let file = iter.next().expect("Archive should contain at least one pattern.");
+      let mut input = BufReader::new(file);
+      let (width, height, moves_count, priority) = Patterns::read_header(&mut input, &mut s);
+      Patterns::read_pattern(&mut input, &mut s, width, height);
+      let cur_dfa = Patterns::build_dfa(width, height, 0, &s);
+      let moves = Patterns::read_moves(&mut input, &mut s, moves_count);
+      patterns.push(Pattern {
+        p: priority,
+        moves: moves
+      });
+      cur_dfa
+    };
+    for file in iter {
+      let mut input = BufReader::new(file);
+      let (width, height, moves_count, priority) = Patterns::read_header(&mut input, &mut s);
+      Patterns::read_pattern(&mut input, &mut s, width, height);
+      let cur_dfa = Patterns::build_dfa(width, height, patterns.len() as u32, &s);
+      dfa = dfa.product(&cur_dfa);
+      dfa.delete_non_reachable();
+      let moves = Patterns::read_moves(&mut input, &mut s, moves_count);
+      patterns.push(Pattern {
+        p: priority,
+        moves: moves
+      });
     }
+    dfa.minimize();
+    Patterns {
+      dfa: dfa,
+      patterns: patterns
+    }
+  }
 
-    fn covering_spiral_length(side_of_square: u32) -> u32 {
-        let x = side_of_square / 2;
-        (8 * x - 13) * x + 6
-    }
-
-    fn build_dfa(width: u32, height: u32, pattern: u32, s: &str) -> Dfa {
-        let center_x = width / 2;
-        let center_y = height / 2;
-        let spiral_length = Patterns::covering_spiral_length(cmp::max(width, height)) as usize;
-        let mut states = Vec::with_capacity(spiral_length + 1);
-        let mut i = 0;
-        for (shift_x, shift_y) in Spiral::new().into_iter().take(spiral_length) {
-            i += 1;
-            let x = center_x as i32 + shift_x;
-            let y = center_y as i32 + shift_y;
-            let state = if x >= 0 && x < width as i32 && y >= 0 && y < height as i32 {
-                let pos = y as u32 * width + x as u32;
-                match s.char_at(pos as usize) {
-                    '.' => DfaState::new(i, -1, -1, -1, -1),
-                    '?' => DfaState::new(i, i, i, i, -1),
-                    'R' => DfaState::new(-1, i, -1, -1, -1),
-                    'B' => DfaState::new(-1, -1, i, -1, -1),
-                    'r' => DfaState::new(i, i, -1, -1, -1),
-                    'b' => DfaState::new(i, -1, i, -1, -1),
-                    '*' => DfaState::new(-1, -1, -1, i, -1),
-                    c   => panic!("Invalid character in pattern: {}", c)
-                }
-                
-            } else {
-                DfaState::new(-1, -1, -1, -1, -1)
-            };
-            states.push(state);
-        }
-        states.push(DfaState::new(-1, -1, -1, -1, pattern as i32));
-        Dfa::new(states)
-    }
-
-    pub fn load(file: File) -> Patterns {
-        let archive = Archive::new(file);
-        let mut s = String::new();
-        let mut patterns = Vec::new();
-        let mut iter = archive.files().expect("Reading of tar archive is failed.").into_iter().map(|file| file.expect("Reading of file in tar archive is failed."));
-        let mut dfa = {
-            let file = iter.next().expect("Archive should contain at least one pattern.");
-            let mut input = BufReader::new(file);
-            let (width, height, moves_count, priority) = Patterns::read_header(&mut input, &mut s);
-            Patterns::read_pattern(&mut input, &mut s, width, height);
-            let cur_dfa = Patterns::build_dfa(width, height, 0, &s);
-            let moves = Patterns::read_moves(&mut input, &mut s, moves_count);
-            patterns.push(Pattern {
-                p: priority,
-                moves: moves
-            });
-            cur_dfa
-        };
-        for file in iter {
-            let mut input = BufReader::new(file);
-            let (width, height, moves_count, priority) = Patterns::read_header(&mut input, &mut s);
-            Patterns::read_pattern(&mut input, &mut s, width, height);
-            let cur_dfa = Patterns::build_dfa(width, height, patterns.len() as u32, &s);
-            dfa = dfa.product(&cur_dfa);
-            dfa.delete_non_reachable();
-            let moves = Patterns::read_moves(&mut input, &mut s, moves_count);
-            patterns.push(Pattern {
-                p: priority,
-                moves: moves
-            });
-        }
-        dfa.minimize();
-        Patterns {
-            dfa: dfa,
-            patterns: patterns
-        }
-    }
-
-    pub fn find(&self, field: &Field) -> Vec<(i32, i32, f64)> {
-        unimplemented!()
-    }
+  pub fn find(&self, field: &Field) -> Vec<(i32, i32, f64)> {
+    unimplemented!()
+  }
 }
