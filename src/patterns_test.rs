095109379f23688ea89956d952b798da0c2cc6c5
Kurnevsky Evgeny <kurnevsky@gmail.com>
2016-08-13 13:28:10 +0300

    Add patterns tests.

--- src/patterns_test.rs
+++ src/patterns_test.rs
@@ -1,20 +1,25 @@
+use player::Player;
 use patterns::Patterns;
+use construct_field::construct_field;
 
-fn construct_patterns(s: &str) -> Patterns {
+fn construct_patterns(strings: &[&str]) -> Patterns {
   let mut pattern_s = String::new();
-  for line in s.split('\n').map(|line| line.trim_matches(' ')).filter(|line| !line.is_empty()) {
-    pattern_s.push_str(line);
-    pattern_s.push('\n');
-  }
   let mut p = Patterns::empty();
-  p.add_str(pattern_s.as_str());
+  for s in strings {
+    pattern_s.clear();
+    for line in s.split('\n').map(|line| line.trim_matches(' ')).filter(|line| !line.is_empty()) {
+      pattern_s.push_str(line);
+      pattern_s.push('\n');
+    }
+    p.add_str(pattern_s.as_str());
+  }
   p
 }
 
 #[test]
 #[should_panic]
 fn pattern_moves_discrepancy() {
-  construct_patterns(
+  construct_patterns(&[
     "
     4 4 1.0
     ....
@@ -23,5 +28,66 @@ fn pattern_moves_discrepancy() {
     ..+.
     3 3 1.0
     "
+  ]);
+}
+
+#[test]
+#[should_panic]
+fn pattern_without_moves_on_image() {
+  construct_patterns(&[
+    "
+    4 4 1.0
+    ....
+    .RB.
+    .BR.
+    ....
+    3 3 1.0
+    "
+  ]);
+}
+
+#[test]
+fn pattern_borders_matches() {
+  let p = construct_patterns(&[
+    "
+    4 4 1.0
+    #...
+    #RB.
+    #BR.
+    #..+
+    3 3 1.0
+    "
+  ]);
+  let field = construct_field(
+    "
+    ...
+    aA.
+    Aa.
+    ...
+    "
   );
+  assert_eq!(p.find(&field, Player::Red, false), vec![(field.to_pos(2, 3), 1f64)]);
+}
+
+#[test]
+fn pattern_borders_doesnt_match() {
+  let p = construct_patterns(&[
+    "
+    4 4 1.0
+    #...
+    #RB.
+    #BR.
+    #..+
+    3 3 1.0
+    "
+  ]);
+  let field = construct_field(
+    "
+    ....
+    .aA.
+    .Aa.
+    ....
+    "
+  );
+  assert!(p.find(&field, Player::Red, false).is_empty());
 }
