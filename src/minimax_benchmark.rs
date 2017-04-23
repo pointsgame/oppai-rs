05cd0d5e238398ac5850574df66200fcc5d2d847
Kurnevsky Evgeny <kurnevsky@gmail.com>
2017-04-23 17:56:55 +0300

    Add new benchs.

--- src/minimax_benchmark.rs
+++ src/minimax_benchmark.rs
@@ -5,7 +5,7 @@ use minimax::minimax;
 use construct_field::construct_field;
 
 #[bench]
-fn find_best_move(bencher: &mut Bencher) {
+fn find_best_move_1(bencher: &mut Bencher) {
   let field = construct_field(
     "
     .............
@@ -25,5 +25,27 @@ fn find_best_move(bencher: &mut Bencher) {
     let mut rng = XorShiftRng::new_unseeded();
     let mut local_field = field.clone();
     minimax(&mut local_field, Player::Black, &mut rng, 6)
+  });
+}
+
+#[bench]
+fn find_best_move_2(bencher: &mut Bencher) {
+  let field = construct_field(
+    "
+    .......
+    ...a...
+    .......
+    ..Aa.A.
+    .A...A.
+    .AaaaA.
+    ..AAAa.
+    .....a.
+    .......
+    "
+  );
+  bencher.iter(|| {
+    let mut rng = XorShiftRng::new_unseeded();
+    let mut local_field = field.clone();
+    minimax(&mut local_field, Player::Red, &mut rng, 6)
   });
 }
