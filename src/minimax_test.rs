ac72d9382aa162b8aef94621f868dc1c4a1d80b2
Evgeny Kurnevsky <kurnevsky@gmail.com>
2017-10-28 16:50:40 +0300

    Correct test, fixes #5.

--- src/minimax_test.rs
+++ src/minimax_test.rs
@@ -5,6 +5,20 @@ use construct_field::construct_field;
 
 #[test]
 fn find_best_move() {
+  // 8 is the minimum depth value to detect correct move in this test.
+  // With depth 7 after 3 moves we might have this position:
+  // ........
+  // .....a..
+  // ...a....
+  // ..AaAAa.
+  // ...Aaa?.
+  // ..A.A?..
+  // ........
+  // ........
+  // Question marks here are indicated trajectory that will be excluded
+  // because it doesn't intersect any other trajectory with length 2.
+  // Without this trajectory black player won't be able to find the escape.
+  // So red player will think that he wins with move (5, 1).
   let mut field = construct_field(
     "
     ........
@@ -18,12 +32,6 @@ fn find_best_move() {
     "
   );
   let mut rng = XorShiftRng::new_unseeded();
-  let pos1 = minimax(&mut field, Player::Red, &mut rng, 5);
-  assert_eq!(pos1, Some(field.to_pos(5, 2)));
-  let pos2 = minimax(&mut field, Player::Red, &mut rng, 6);
-  assert_eq!(pos2, Some(field.to_pos(5, 2)));
-  let pos3 = minimax(&mut field, Player::Red, &mut rng, 7);
-  assert_eq!(pos3, Some(field.to_pos(5, 2)));
-  let pos4 = minimax(&mut field, Player::Red, &mut rng, 8);
-  assert_eq!(pos4, Some(field.to_pos(5, 2)));
+  let pos = minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 2)));
 }
