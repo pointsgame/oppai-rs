c0d0ff74c37310db7976238f659f21e4bca61f2c
Evgeny Kurnevsky <kurnevsky@gmail.com>
2019-09-21 14:38:17 +0300

    Use criterion for field benchmarks.

--- oppai-field/src/field_benchmark.rs
+++ oppai-field/src/field_benchmark.rs
@@ -1,28 +1,31 @@
 #[macro_use]
 extern crate criterion;
-extern crate oppai;
+extern crate oppai_field;
 
 use criterion::black_box;
+use criterion::Bencher;
 use criterion::Criterion;
-
-use oppai::fibonacci;
-
-use field::{self, Field, Pos};
-use player::Player;
-use rand::{Rng, SeedableRng, XorShiftRng};
+use oppai_field::field::{self, Field, Pos};
+use oppai_field::player::Player;
+use oppai_field::zobrist::Zobrist;
+use rand::seq::SliceRandom;
+use rand::SeedableRng;
+use rand_xorshift::XorShiftRng;
 use std::sync::Arc;
-use test::Bencher;
-use zobrist::Zobrist;
 
-fn random_game(bencher: &mut Bencher, width: u32, height: u32, seed_array: [u32; 4]) {
+const SEED_1: [u8; 16] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53];
+const SEED_2: [u8; 16] = [23, 29, 31, 37, 41, 43, 47, 53, 2, 3, 5, 7, 11, 13, 17, 19];
+const SEED_3: [u8; 16] = [11, 13, 17, 19, 23, 29, 31, 37, 2, 3, 5, 7, 41, 43, 47, 53];
+
+fn random_game(bencher: &mut Bencher, width: u32, height: u32, seed_array: [u8; 16]) {
   let mut rng = XorShiftRng::from_seed(seed_array);
   let mut moves = (field::min_pos(width)..field::max_pos(width, height) + 1).collect::<Vec<Pos>>();
-  rng.shuffle(&mut moves);
+  moves.shuffle(&mut rng);
   let zobrist = Arc::new(Zobrist::new(field::length(width, height) * 2, &mut rng));
   bencher.iter(|| {
     let mut field = Field::new(width, height, zobrist.clone());
     let mut player = Player::Red;
-    for &pos in &moves {
+    for &pos in black_box(&moves) {
       if field.is_putting_allowed(pos) {
         field.put_point(pos, player);
         player = player.next();
@@ -35,17 +38,23 @@ fn random_game(bencher: &mut Bencher, width: u32, height: u32, seed_array: [u32;
   });
 }
 
-#[bench]
-fn random_game_1(bencher: &mut Bencher) {
-  random_game(bencher, 100, 100, [3, 1, 7, 5]);
+pub fn random_game_1(c: &mut Criterion) {
+  c.bench_function("random_game_1", |bencher| {
+    random_game(bencher, 30, 30, SEED_1)
+  });
 }
 
-#[bench]
-fn random_game_2(bencher: &mut Bencher) {
-  random_game(bencher, 100, 100, [1, 3, 5, 7]);
+pub fn random_game_2(c: &mut Criterion) {
+  c.bench_function("random_game_2", |bencher| {
+    random_game(bencher, 30, 30, SEED_2)
+  });
 }
 
-#[bench]
-fn random_game_3(bencher: &mut Bencher) {
-  random_game(bencher, 100, 100, [7, 1, 3, 5]);
+pub fn random_game_3(c: &mut Criterion) {
+  c.bench_function("random_game_3", |bencher| {
+    random_game(bencher, 30, 30, SEED_3)
+  });
 }
+
+criterion_group!(random_games, random_game_1, random_game_2, random_game_3);
+criterion_main!(random_games);
