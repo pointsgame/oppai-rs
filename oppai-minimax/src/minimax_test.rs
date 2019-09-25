e5fa9f28f149443f4fe0849d18dbb29afa70f32c
Evgeny Kurnevsky <kurnevsky@gmail.com>
2019-09-25 21:36:58 +0300

    Add tests for minimax.

--- oppai-minimax/src/minimax_test.rs
+++ oppai-minimax/src/minimax_test.rs
@@ -11,7 +11,7 @@ const MINIMAX_CONFIG_NEGASCOUT: MinimaxConfig = MinimaxConfig {
   threads_count: 1,
   minimax_type: MinimaxType::NegaScout,
   minimax_moves_sorting: MinimaxMovesSorting::TrajectoriesCount,
-  hash_table_size: 1_000,
+  hash_table_size: 10_000,
   rebuild_trajectories: false,
 };
 
@@ -19,12 +19,12 @@ const MINIMAX_CONFIG_MTDF: MinimaxConfig = MinimaxConfig {
   threads_count: 1,
   minimax_type: MinimaxType::MTDF,
   minimax_moves_sorting: MinimaxMovesSorting::TrajectoriesCount,
-  hash_table_size: 1_000,
+  hash_table_size: 10_000,
   rebuild_trajectories: false,
 };
 
 #[test]
-fn find_best_move() {
+fn find_best_move_1() {
   env_logger::try_init().ok();
   // 8 is the minimum depth value to detect correct move in this test.
   // With depth 7 after 3 moves we might have this position:
@@ -60,4 +60,356 @@ fn find_best_move() {
   let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
   let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
   assert_eq!(pos, Some(field.to_pos(5, 2)));
+}
+
+
+#[test]
+fn find_best_move_2() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ........
+    ........
+    ...a.a..
+    ...AAa..
+    ...aAa..
+    ....Aa..
+    ...aaA..
+    ........
+    ........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(2, 3)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(2, 3)));
+}
+
+#[test]
+fn find_best_move_3() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ........
+    ........
+    ...a....
+    ..aA.a..
+    ..aAA...
+    ..aa....
+    ........
+    ........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 5)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 5)));
+}
+
+#[test]
+fn find_best_move_4() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    .........
+    ....a....
+    .........
+    ...Aa.A..
+    ..A...A..
+    ..AaaaA..
+    ...AAAa..
+    ......a..
+    .........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+}
+
+#[test]
+fn find_best_move_5() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ...........
+    ....aaa....
+    ..AAa.A.A..
+    .A.aAA...A.
+    ...a.......
+    ...a..a....
+    ....aa.....
+    ...........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(6, 3)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(6, 3)));
+}
+
+#[test]
+fn find_best_move_6() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ............
+    ............
+    ..A.a.......
+    ...Aa..aa...
+    ...aAaaaAA..
+    ...aAAaA....
+    ...a.A......
+    ............
+    ............
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(7, 6)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(7, 6)));
+}
+
+#[test]
+#[ignore]
+fn find_best_move_7() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ............
+    .......aa...
+    .a...AaA.a..
+    ..a.A.A.Aa..
+    ..a..A.A.a..
+    ...aaaaaa...
+    ............
+    ............
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 10);
+  assert_eq!(pos, Some(field.to_pos(4, 1)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 10);
+  assert_eq!(pos, Some(field.to_pos(4, 1)));
+}
+
+#[test]
+fn find_best_move_8() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ............
+    ............
+    .......AA...
+    .....AAaaa..
+    .....Aa.....
+    ..A.Aa.a....
+    ...Aa.A..a..
+    ..Aa.a......
+    ..Aa.a..A...
+    ...AAAAA....
+    ............
+    ............
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(6, 7)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(6, 7)));
+}
+
+#[test]
+#[ignore]
+fn find_best_move_9() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ...........
+    ...........
+    ...aA...a..
+    ..aA...a...
+    ..aAA.a....
+    ..aAAAAa...
+    ..aaAaaA...
+    ..AAaaAA...
+    ....a......
+    ...AaA.....
+    ....A......
+    ...........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 10);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 10);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+}
+
+#[test]
+fn find_best_move_10() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ..........
+    ..........
+    ....aaaA..
+    .....AAa..
+    ..A..A.a..
+    ...A..a...
+    ....A.a...
+    .....Aa...
+    ....Aa.a..
+    ....Aa....
+    ..........
+    ..........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 6)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 6)));
+}
+
+#[test]
+#[ignore]
+fn find_best_move_11() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ...........
+    ...........
+    ..A........
+    ..A........
+    ..A...Aaa..
+    ...AaaaA...
+    ....AAA....
+    ...........
+    ...........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 12);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 12);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+}
+
+#[test]
+fn find_best_move_12() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ...........
+    ...........
+    ...a..a....
+    ...AA.aAA..
+    ...a.AAa...
+    ...aaAaa...
+    ..AAAa.....
+    .....a.....
+    ...........
+    ...........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(5, 3)));
+}
+
+#[test]
+fn find_best_move_13() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    .........
+    .........
+    ...AA.A..
+    ...Aaa...
+    ...Aa.A..
+    ..aaAA...
+    ....aa...
+    .........
+    .........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(6, 5)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(6, 5)));
+}
+
+#[test]
+fn find_best_move_14() {
+  env_logger::try_init().ok();
+  let mut rng = XorShiftRng::from_seed(SEED);
+  let mut field = construct_field(
+    &mut rng,
+    "
+    ..........
+    ..........
+    ...aa.....
+    ..a..a....
+    ..a...a...
+    ..aAA.Aa..
+    ..Aa..Aa..
+    .....A.a..
+    ...AA..a..
+    ......a...
+    ..........
+    ..........
+    ",
+  );
+  let minimax = Minimax::new(MINIMAX_CONFIG_NEGASCOUT);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(4, 7)));
+  let minimax = Minimax::new(MINIMAX_CONFIG_MTDF);
+  let pos = minimax.minimax(&mut field, Player::Red, &mut rng, 8);
+  assert_eq!(pos, Some(field.to_pos(4, 7)));
 }
