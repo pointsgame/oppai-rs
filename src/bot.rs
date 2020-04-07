85d5e295c07486e152b774db7b472fc15a555088
Evgeny Kurnevsky <kurnevsky@gmail.com>
2020-04-07 21:42:36 +0300

    Apply rustfmt.

--- src/bot.rs
+++ src/bot.rs
@@ -140,12 +140,21 @@ impl Bot {
     match self.config.bot.solver {
       Solver::Uct => self
         .uct
-        .best_move_with_time(&self.field, player, &mut self.rng, Duration::from_millis(u64::from(time - self.config.bot.time_gap)))
+        .best_move_with_time(
+          &self.field,
+          player,
+          &mut self.rng,
+          Duration::from_millis(u64::from(time - self.config.bot.time_gap)),
+        )
         .or_else(|| heuristic::heuristic(&self.field, player))
         .map(|pos| (self.field.to_x(pos.get()), self.field.to_y(pos.get()))),
       Solver::Minimax => self
         .minimax
-        .minimax_with_time(&mut self.field, player, Duration::from_millis(u64::from(time - self.config.bot.time_gap)))
+        .minimax_with_time(
+          &mut self.field,
+          player,
+          Duration::from_millis(u64::from(time - self.config.bot.time_gap)),
+        )
         .or_else(|| heuristic::heuristic(&self.field, player))
         .map(|pos| (self.field.to_x(pos.get()), self.field.to_y(pos.get()))),
       Solver::Heuristic => {
