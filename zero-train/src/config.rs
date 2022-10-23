3a74be251e44d5bfc11a00b034f8cfd71b929a39
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-10-23 22:49:39 +0300

    Add config.

---
+++ zero-train/src/config.rs
@@ -0,0 +1,39 @@
+use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, Command};
+
+pub struct Config {
+  pub width: u32,
+  pub height: u32,
+}
+
+impl Default for Config {
+  fn default() -> Self {
+    Self { width: 16, height: 16 }
+  }
+}
+
+pub fn cli_parse() -> Config {
+  let matches = Command::new(crate_name!())
+    .version(crate_version!())
+    .author(crate_authors!("\n"))
+    .about(crate_description!())
+    .arg(
+      Arg::new("width")
+        .long("width")
+        .help("Field width")
+        .takes_value(true)
+        .default_value("16"),
+    )
+    .arg(
+      Arg::new("height")
+        .long("height")
+        .help("Field height")
+        .takes_value(true)
+        .default_value("16"),
+    )
+    .get_matches();
+
+  let width = matches.value_of_t("width").unwrap_or_else(|e| e.exit());
+  let height = matches.value_of_t("height").unwrap_or_else(|e| e.exit());
+
+  Config { width, height }
+}
--- zero-train/src/main.rs
+++ zero-train/src/main.rs
@@ -1,5 +1,8 @@
+mod config;
+
 use std::{path::PathBuf, sync::Arc};
 
+use config::cli_parse;
 use oppai_field::{
   field::{length, Field},
   player::Player,
@@ -15,15 +18,14 @@ fn main() -> PyResult<()> {
   let env = env_logger::Env::default().filter_or("RUST_LOG", "info");
   env_logger::Builder::from_env(env).init();
 
-  let width = 16;
-  let height = 16;
+  let config = cli_parse();
   let player = Player::Red;
 
   let mut rng = SmallRng::from_entropy();
-  let zobrist = Arc::new(Zobrist::new(length(width, height) * 2, &mut rng));
-  let mut field = Field::new(width, height, zobrist);
+  let zobrist = Arc::new(Zobrist::new(length(config.width, config.height) * 2, &mut rng));
+  let mut field = Field::new(config.width, config.height, zobrist);
 
-  for (pos, player) in InitialPosition::Cross.points(width, height, player) {
+  for (pos, player) in InitialPosition::Cross.points(config.width, config.height, player) {
     // TODO: random shift
     field.put_point(pos, player);
   }
@@ -33,7 +35,7 @@ fn main() -> PyResult<()> {
   if exists {
     log::info!("Loading the model from {}", path.display());
   }
-  let model = PyModel::new(path, width, height, 4)?;
+  let model = PyModel::new(path, config.width, config.height, 4)?;
   if exists {
     model.load()?;
   }
