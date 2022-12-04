58ee05ba91d36ed806cb84bd2de1527cca886ac3
Evgeny Kurnevsky <kurnevsky@gmail.com>
2022-12-04 10:23:26 +0300

    Update dlprim.

--- flake.nix
+++ flake.nix
@@ -19,26 +19,7 @@
     let
       pkgs = import inputs.nixpkgs {
         system = "x86_64-linux";
-        overlays = [
-          inputs.fenix.overlay
-          (self: super: {
-            python3 = super.python3.override {
-              packageOverrides = python-self: python-super: {
-                pytorch = python-super.pytorch.overrideAttrs (old: {
-                  patches = old.patches ++ [
-                    (pkgs.fetchpatch {
-                      url =
-                        "https://github.com/pytorch/pytorch/commit/eb74af18af6e90ae47f24997af8468bf7b9deb72.patch";
-                      sha256 =
-                        "sha256-IRmBW05naNEWBttiRfURm6jGd7UpsYpNiwqxkiGn7l4=";
-                    })
-                  ];
-                });
-              };
-            };
-            python3Packages = self.python3.pkgs;
-          })
-        ];
+        overlays = [ inputs.fenix.overlays.default ];
       };
     in {
       devShell.x86_64-linux = pkgs.mkShell {
@@ -55,12 +36,7 @@
           rust-analyzer
         ];
 
-        buildInputs = let
-          dlprimitives = (pkgs.callPackage ./dlprimitives.nix { });
-          pytorch-dlprim = (pkgs.callPackage ./pytorch-dlprim.nix {
-            dlprimitives = dlprimitives;
-          });
-        in with pkgs; [
+        buildInputs = with pkgs; [
           atk
           cairo
           freetype
@@ -69,11 +45,8 @@
           gtk3
           pango
           vulkan-loader
-          ((python3.withPackages
-            (pkgs: with pkgs; [ pytorch torchvision ])).override
-            (args: { ignoreCollisions = true; }))
-          dlprimitives
-          pytorch-dlprim
+          (python3.withPackages (pkgs: with pkgs; [ pytorch torchvision ]))
+          (pkgs.callPackage ./pytorch-dlprim.nix { })
         ];
 
         LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
--- pytorch-dlprim.nix
+++ pytorch-dlprim.nix
@@ -1,5 +1,4 @@
-{ stdenv, fetchFromGitHub, cmake, python3Packages, ocl-icd, opencl-clhpp
-, dlprimitives }:
+{ stdenv, fetchFromGitHub, cmake, python3Packages, ocl-icd, opencl-clhpp }:
 
 stdenv.mkDerivation {
   pname = "pytorch-dlprim";
@@ -8,15 +7,18 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "artyom-beilis";
     repo = "pytorch_dlprim";
-    sha256 = "sha256-et7psx+WP7KR01Op4cMJXdDNa9xY6zpYccJgedpRohg=";
-    rev = "525956ab8fd549210aaafdc0908ee31afaf99211";
+    sha256 = "sha256-y8MuWPmz/uIxHyhfzNf24Ru04xg4ypanRXJoQmBteok=";
+    rev = "32da36d266c52250cf0fa54cdd30b847d568af2d";
+    fetchSubmodules = true;
   };
 
   installPhase = ''
     mkdir -p $out/lib
+    cp dlprimitives/libdlprim_core.so $out/lib
+    patchelf --set-rpath "$out/lib:''$(patchelf --print-rpath libpt_ocl.so)" libpt_ocl.so
     cp libpt_ocl.so $out/lib
   '';
 
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ ocl-icd opencl-clhpp python3Packages.torch dlprimitives ];
+  buildInputs = [ ocl-icd opencl-clhpp python3Packages.torch ];
 }
