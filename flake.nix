c5cff992524f0e2f66d1396240c990efabe5d0fe
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-05-21 20:48:38 +0300

    Use flake-utils.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,12 @@
       ref = "nixos-unstable";
     };
 
+    flake-utils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+    };
+
     fenix = {
       type = "github";
       owner = "nix-community";
@@ -16,44 +22,45 @@
   };
 
   outputs = inputs:
-    let
-      pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ inputs.fenix.overlays.default ];
-      };
-    in {
-      devShell.x86_64-linux = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          cmake
-          pkg-config
-          (fenix.latest.withComponents [
-            "cargo"
-            "clippy"
-            "rust-src"
-            "rustc"
-            "rustfmt"
-          ])
-          rust-analyzer
-        ];
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ inputs.fenix.overlays.default ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+            (fenix.latest.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            rust-analyzer
+          ];
 
-        buildInputs = with pkgs; [
-          atk
-          cairo
-          freetype
-          gdk-pixbuf
-          glib
-          gtk3
-          librsvg
-          pango
-          vulkan-loader
-          (python3.withPackages (pkgs: with pkgs; [ pytorch torchvision ]))
-          (pkgs.callPackage ./pytorch-dlprim.nix { })
-        ];
+          buildInputs = with pkgs; [
+            atk
+            cairo
+            freetype
+            gdk-pixbuf
+            glib
+            gtk3
+            librsvg
+            pango
+            vulkan-loader
+            (python3.withPackages (pkgs: with pkgs; [ pytorch torchvision ]))
+            (pkgs.callPackage ./pytorch-dlprim.nix { })
+          ];
 
-        LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
-        XDG_DATA_DIRS =
-          "${pkgs.gtk3}/share/gsettings-schemas/gtk+3-${pkgs.gtk3.version}:"
-          + "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/gsettings-desktop-schemas-${pkgs.gsettings-desktop-schemas.version}";
-      };
-    };
+          LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
+          XDG_DATA_DIRS =
+            "${pkgs.gtk3}/share/gsettings-schemas/gtk+3-${pkgs.gtk3.version}:"
+            + "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/gsettings-desktop-schemas-${pkgs.gsettings-desktop-schemas.version}";
+        };
+      });
 }
