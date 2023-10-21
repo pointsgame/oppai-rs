a7c954f8ca6cec18b2b8e37814709631ec0e2ca4
Evgeny Kurnevsky <kurnevsky@gmail.com>
2023-10-21 14:11:56 +0300

    Save captures.

--- sgf/src/lib.rs
+++ sgf/src/lib.rs
@@ -1,15 +1,33 @@
 #[cfg(test)]
 mod test;
 
-use oppai_field::{any_field::AnyField, field::Field, player::Player};
+use oppai_field::{any_field::AnyField, extended_field::ExtendedField, player::Player};
 use rand::Rng;
 use sgf_parse::{serialize, unknown_game::Prop, GameTree, SgfNode};
-use std::iter;
+use std::{fmt::Display, iter};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 enum Move {
   Pass,
-  Move(u8, u8, Vec<(u8, u8)>),
+  Move(u8, u8, Vec<Vec<(u8, u8)>>),
+}
+
+impl Display for Move {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      Move::Pass => Ok(()),
+      Move::Move(x, y, chains) => {
+        write!(f, "{}{}", from_coordinate(*x) as char, from_coordinate(*y) as char)?;
+        for chain in chains {
+          write!(f, ".")?;
+          for (x, y) in chain {
+            write!(f, "{}{}", from_coordinate(*x) as char, from_coordinate(*y) as char)?;
+          }
+        }
+        Ok(())
+      }
+    }
+  }
 }
 
 fn to_coordinate(c: u8) -> u8 {
@@ -36,21 +54,23 @@ fn parse_move(s: &str) -> Option<Move> {
   }
   let x = to_coordinate(s.as_bytes()[0]);
   let y = to_coordinate(s.as_bytes()[1]);
-  let mut chain = Vec::new();
+  let mut chains = Vec::new();
   if s.len() > 2 {
-    if s.as_bytes()[2] != b'.' || s.len() % 2 == 0 {
-      return None;
-    }
+    for c in s.split('.').skip(1) {
+      let mut chain = Vec::new();
+      if c.len() % 2 == 1 {
+        return None;
+      }
 
-    let mut i = 3;
-    while i < s.len() {
-      let x = to_coordinate(s.as_bytes()[i]);
-      let y = to_coordinate(s.as_bytes()[i + 1]);
-      chain.push((x, y));
-      i += 2;
+      for i in 0..c.len() / 2 {
+        let x = to_coordinate(c.as_bytes()[i * 2]);
+        let y = to_coordinate(c.as_bytes()[i * 2 + 1]);
+        chain.push((x, y));
+      }
+      chains.push(chain);
     }
   }
-  Some(Move::Move(x, y, chain))
+  Some(Move::Move(x, y, chains))
 }
 
 pub fn from_sgf<F: AnyField, R: Rng>(sgf: &str, rng: &mut R) -> Option<F> {
@@ -73,10 +93,10 @@ pub fn from_sgf<F: AnyField, R: Rng>(sgf: &str, rng: &mut R) -> Option<F> {
   let mut field = <F as AnyField>::new_from_rng(width as u32, height as u32, rng);
 
   let mut handle = |player: Player, s: &str| -> bool {
-    if let Some(Move::Move(x, y, chain)) = parse_move(s) {
+    if let Some(Move::Move(x, y, chains)) = parse_move(s) {
       let pos = field.field().to_pos(x as u32, y as u32);
       let result = field.put_players_point(pos, player);
-      if !chain.into_iter().all(|(x, y)| {
+      if !chains.into_iter().flat_map(|chain| chain.into_iter()).all(|(x, y)| {
         let pos = field.field().to_pos(x as u32, y as u32);
         field.field().cell(pos).is_bound_player(player)
       }) {
@@ -123,17 +143,46 @@ pub fn from_sgf<F: AnyField, R: Rng>(sgf: &str, rng: &mut R) -> Option<F> {
   Some(field)
 }
 
-pub fn to_sgf(field: &Field) -> Option<String> {
-  if field.width() > 52 || field.height() > 52 {
+pub fn to_sgf(field: &ExtendedField) -> Option<String> {
+  if field.field().width() > 52 || field.field().height() > 52 {
     return None;
   }
 
   let mut node = SgfNode::new(Vec::new(), Vec::new(), false);
-  for &pos in field.points_seq().iter().rev() {
-    let x = field.to_x(pos) as u8;
-    let y = field.to_y(pos) as u8;
-    let m = format!("{}{}", from_coordinate(x) as char, from_coordinate(y) as char);
-    match field.cell(pos).get_player() {
+  let mut i = field.captures.len();
+  for (n, &pos) in field.field().points_seq().iter().enumerate().rev() {
+    let x = field.field().to_x(pos) as u8;
+    let y = field.field().to_y(pos) as u8;
+    let player = field.field().cell(pos).get_player();
+    let mut chains = Vec::new();
+    while i > 0 && field.captures[i - 1].2 > n + 1 {
+      i -= 1;
+    }
+    if i > 0 && field.captures[i - 1].1 == player && field.captures[i - 1].2 == n + 1 {
+      chains.push(
+        field.captures[i - 1]
+          .0
+          .iter()
+          .map(|&pos| (field.field().to_x(pos) as u8, field.field().to_y(pos) as u8))
+          .collect(),
+      )
+    }
+    if n > 0 && field.field().cell(field.field().points_seq()[n - 1]).get_player() == player.next() {
+      for j in 0..2 {
+        if i > j && field.captures[i - j - 1].1 == player && field.captures[i - j - 1].2 == n {
+          chains.push(
+            field.captures[i - j - 1]
+              .0
+              .iter()
+              .map(|&pos| (field.field().to_x(pos) as u8, field.field().to_y(pos) as u8))
+              .collect(),
+          )
+        }
+      }
+    }
+    let m = Move::Move(x, y, chains);
+    let m = format!("{}", m);
+    match player {
       Player::Red => node.properties.push(Prop::W(m)),
       Player::Black => node.properties.push(Prop::B(m)),
     }
@@ -142,7 +191,8 @@ pub fn to_sgf(field: &Field) -> Option<String> {
   node.properties.push(Prop::GM(40));
   node
     .properties
-    .push(Prop::SZ((field.width() as u8, field.height() as u8)));
+    .push(Prop::SZ((field.field().width() as u8, field.field().height() as u8)));
+  node.properties.push(Prop::RU("russian".into()));
   node.is_root = true;
   let tree = GameTree::Unknown(node);
 
--- sgf/src/test.rs
+++ sgf/src/test.rs
@@ -1,5 +1,5 @@
 use crate::{from_sgf, to_sgf};
-use oppai_field::{construct_field::construct_field, field::Field, player::Player};
+use oppai_field::{any_field::AnyField, construct_field::construct_field, field::Field, player::Player};
 use rand::SeedableRng;
 use rand_xoshiro::Xoshiro256PlusPlus;
 
@@ -17,11 +17,78 @@ fn cross() {
     .Dc.
     ....
     ",
-  );
+  )
+  .into();
   let sgf = to_sgf(&field).unwrap();
-  assert_eq!(sgf, "(;GM[40]SZ[4:4];W[bb];B[cb];W[cc];B[bc])");
+  assert_eq!(sgf, "(;GM[40]SZ[4:4]RU[russian];W[bb];B[cb];W[cc];B[bc])");
   let field_from_sgf: Field = from_sgf(sgf.as_ref(), &mut rng).unwrap();
-  assert_eq!(field_from_sgf.points_seq(), field.points_seq());
+  assert_eq!(field_from_sgf.points_seq(), field.field().points_seq());
+}
+
+#[test]
+fn simple_surround() {
+  env_logger::try_init().ok();
+  let mut rng = Xoshiro256PlusPlus::seed_from_u64(SEED);
+  let field = construct_field(
+    &mut rng,
+    "
+    .a.
+    cBa
+    .a.
+    ",
+  )
+  .into();
+  let sgf = to_sgf(&field).unwrap();
+  assert_eq!(
+    sgf,
+    "(;GM[40]SZ[3:3]RU[russian];W[ba];W[cb];W[bc];B[bb];W[ab.abbccbba])"
+  );
+  let field_from_sgf: Field = from_sgf(sgf.as_ref(), &mut rng).unwrap();
+  assert_eq!(field_from_sgf.points_seq(), field.field().points_seq());
+}
+
+#[test]
+fn apply_control_surrounding_in_same_turn() {
+  env_logger::try_init().ok();
+  let mut rng = Xoshiro256PlusPlus::seed_from_u64(SEED);
+  let field = construct_field(
+    &mut rng,
+    "
+    .a...
+    aBa.c
+    .a...
+    ",
+  )
+  .into();
+  let sgf = to_sgf(&field).unwrap();
+  assert_eq!(
+    sgf,
+    "(;GM[40]SZ[5:3]RU[russian];W[ba];W[ab];W[cb];W[bc];B[bb];W[eb.abbccbba])"
+  );
+  let field_from_sgf: Field = from_sgf(sgf.as_ref(), &mut rng).unwrap();
+  assert_eq!(field_from_sgf.points_seq(), field.field().points_seq());
+}
+
+#[test]
+fn apply_control_surrounding_in_same_turn_followed_by_simple_surrounding() {
+  env_logger::try_init().ok();
+  let mut rng = Xoshiro256PlusPlus::seed_from_u64(SEED);
+  let field = construct_field(
+    &mut rng,
+    "
+    .a...a.
+    aBa.cAa
+    .a...a.
+    ",
+  )
+  .into();
+  let sgf = to_sgf(&field).unwrap();
+  assert_eq!(
+    sgf,
+    "(;GM[40]SZ[7:3]RU[russian];B[fb];W[ba];W[fa];W[ab];W[cb];W[gb];W[bc];W[fc];B[bb];W[eb.ebfcgbfa.abbccbba])"
+  );
+  let field_from_sgf: Field = from_sgf(sgf.as_ref(), &mut rng).unwrap();
+  assert_eq!(field_from_sgf.points_seq(), field.field().points_seq());
 }
 
 #[test]
