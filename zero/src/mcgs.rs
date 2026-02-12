2888cf9c3c97daea4bdb18155db0080d1fe4be59
Evgeny Kurnevsky <kurnevsky@gmail.com>
2026-02-12 18:21:27 +0100

    Increase virtual loss weight.

--- zero/src/mcgs.rs
+++ zero/src/mcgs.rs
@@ -154,7 +154,9 @@ impl<N: Float + Sum> Search<N> {
 
       // Child value is from child's perspective.
       // Parent wants to maximize own value, which is -child.value
-      let q = -child.value;
+      let visits = N::from(edge.visits).unwrap();
+      let virtual_losses = N::from(edge.virtual_losses).unwrap();
+      let q = (-child.value * visits - virtual_losses) / (visits + virtual_losses);
       let n = edge.visits + edge.virtual_losses;
       let p = edge.prior;
 
